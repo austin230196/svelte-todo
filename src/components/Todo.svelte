a59b8ebc4274ed6c5174a09f27e882901b3aafc9
austin230196 <austinakamelu55@gmail.com>
2024-05-23 13:22:05 +0100

    Added animations 001

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -28,7 +28,7 @@
 </script>
 
 
-<div key={todo.id} class="w-full border-2 mb-2 border-gray-300 rounded-md flex items-center gap-4 p-2 cursor-pointer">
+<div class="w-full border-2 mb-2 border-gray-300 rounded-md flex items-center gap-4 p-2 cursor-pointer">
     <input type="checkbox" checked={todo.completed} on:change={e => changeTodoHandler(e, todo.id)} />
     <section class="">
         <h3 class:completed={todo.completed}>{todo.title}</h3>
